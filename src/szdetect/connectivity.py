6d04e3e81a466b18125483c88f9e09d4ac235393
isabelsarzo <isabel.sarzo@gmail.com>
2025-01-06 15:45:44 -0500

    Added transitivity, eigenvalues, and upper right triang corr coeffs

--- src/szdetect/connectivity.py
+++ src/szdetect/connectivity.py
@@ -21,7 +21,6 @@ def node_degree(CM: np.ndarray) -> np.ndarray:
     return ndeg
 
 def node_strength(CM: np.ndarray) -> np.ndarray:
-    # TODO: use signed approach and return dictionary?
     """
     Computes weighted node degree (node strengths) from undirected connectivity matrix
 
@@ -52,8 +51,64 @@ def clustering_coef(CM: np.ndarray) -> np.ndarray:
 
     """
     d = np.array(np.sum(np.logical_not(CM == 0), axis=1), dtype=float) # computes node degree
-    cube_root = np.sign(CM) * np.abs(CM)**(1 / 3) # handles negative weights
+    cube_root = np.sign(CM) * np.abs(CM)**(1 / 3) # handles negative weights just in case
     cycles3 = np.diag(np.dot(cube_root, np.dot(cube_root, cube_root)))
     d[np.where(cycles3 == 0)] = np.inf  # set coef to 0 when no 3-cycles exist
     ccoef = cycles3 / (d * (d - 1))
     return ccoef
+
+def transitivity(CM: np.ndarray) -> np.float64:
+    """
+    Computes transitivity as the ratio of triangles to triplets in the network,
+    where a triangle refers to a closed triplet (nodes that are connected by three connections)
+
+    Args:
+    ---------------
+    CM: NxN np.ndarray, undirected weighted connectivity matrix
+
+    Returns:
+    ---------------
+    t: np.float64, transitivity ratio
+
+    """
+    d = np.sum(np.logical_not(CM == 0), axis=1)
+    cube_root = np.sign(CM) * np.abs(CM)**(1 / 3) # handles negative weights just in case
+    cycles3 = np.diag(np.dot(cube_root, np.dot(cube_root, cube_root)))
+    t = np.sum(cycles3, axis=0) / np.sum(d * (d - 1), axis=0)
+    return t
+
+def eigenvalues(CM: np.ndarray) -> np.ndarray:
+    """
+    Computes the eigenvalues of the connectivity (correlation) matrix CM
+
+    Args:
+    ---------------
+    CM: NxN np.ndarray, undirected weighted connectivity matrix
+
+    Returns:
+    ---------------
+    eigen_values: Nx1 np.ndarray, eigenvalues of CM
+
+    """
+    eigen_values, eigen_vectors = np.linalg.eig(CM)
+    eigen_values = np.absolute(eigen_values)
+    return eigen_values
+
+def upper_right_triangle(CM: np.ndarray) -> np.ndarray:
+    """
+    Extracts the upper right triangle of the correlation coefficients (connectivity matrix CM)
+    This includes the diagonal
+
+    Args:
+    ---------------
+    CM: NxN np.ndarray, undirected weighted connectivity matrix
+
+    Returns:
+    ---------------
+    corr: ((N*(N+1))/2)x1 np.ndarray, correlation coefficients in the upper right triangle of the matrix CM
+          e.g. For a CM of shape 19x19, corr will have a shape of 190x1
+          
+    """
+    mask = np.triu(np.ones(CM.shape, dtype=bool))
+    corr = CM[mask]
+    return corr
--- tests/test_connectivity.py
+++ tests/test_connectivity.py
@@ -1,4 +1,3 @@
-#import pytest
 from pathlib import Path
 import numpy as np
 from szdetect import connectivity
@@ -24,13 +23,33 @@ def test_clustering_coef(CM):
     #print(ccoef)
     return ccoef
 
+def test_transitivity(CM):
+    assert CM.ndim == 2, f"Expected 2 dimensions, got {CM.ndim}"   
+    t = connectivity.transitivity(CM) 
+    #print(t)
+    return t
+
+def test_eigenvalues(CM):
+    assert CM.ndim == 2, f"Expected 2 dimensions, got {CM.ndim}"   
+    eigen_values = connectivity.eigenvalues(CM) 
+    #print(eigen_values)
+    return eigen_values
+
+def test_upper_right_triangle(CM):
+    assert CM.ndim == 2, f"Expected 2 dimensions, got {CM.ndim}"   
+    corr = connectivity.upper_right_triangle(CM) 
+    #print(corr)
+    return corr
+
 ndeg_list = [test_node_degree(m) for m in CM] 
 print(f"Length of ndeg_list: {len(ndeg_list)}")
 print(f"Type of items in list: {type(ndeg_list[0])}")
 print(f"Shape of items in list: {ndeg_list[0].shape}")
-# TODO: is this implementation correct? There are no zeros,
-# so of course the nodal degree will always be 18 for a 19x19 CM,
-# meaning that each node is connected to all other 18 nodes
+# NOTE: not sure if this implementation is correct or maybe the
+# feature is not very important? 
+# Using the example conn matrices of shape 19x19,
+# I always get the same result: an 19x1 array of 18s. 
+# Is this expected? Since all values are non-zero except for the diagonal
 
 strg_list = [test_node_strength(m) for m in CM]
 print(f"Length of strg_list: {len(strg_list)}")
@@ -41,3 +60,19 @@ ccoef_list = [test_clustering_coef(m) for m in CM]
 print(f"Length of ccoef_list: {len(ccoef_list)}")
 print(f"Type of items in list: {type(ccoef_list[0])}")
 print(f"Shape of items in list: {ccoef_list[0].shape}")
+
+t_list = [test_transitivity(m) for m in CM] 
+print(f"Length of t_list: {len(t_list)}")
+print(f"Type of items in list: {type(t_list[0])}")
+print(f"Shape of items in list: {t_list[0].shape}")
+
+eigenvalue_list = [test_eigenvalues(m) for m in CM]
+print(f"Length of eigenvalue_list: {len(eigenvalue_list)}")
+print(f"Type of items in list: {type(eigenvalue_list[0])}")
+print(f"Shape of items in list: {eigenvalue_list[0].shape}")
+
+corr_list = [test_upper_right_triangle(m) for m in CM]
+print(f"Length of eigenvalue_list: {len(corr_list)}")
+print(f"Type of items in list: {type(corr_list[0])}")
+print(f"Shape of items in list: {corr_list[0].shape}")
+
