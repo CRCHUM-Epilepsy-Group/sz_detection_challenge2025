11595cd625f60b77d30e2214efc46280d6ab6ac6
emilelemoine <lemoine.emile@gmail.com>
2025-01-08 15:53:42 -0500

    Cleanup and use settings.py file

--- 03-extract_labels.py
+++ 03-extract_labels.py
@@ -14,7 +14,7 @@ sys.modules["pull_data"] = pull_data
 spec.loader.exec_module(pull_data)
 
 
-def get_events_info(tsv_file:str):
+def get_events_info(tsv_file: str):
     """
         Retrieve info about EEG recording file from a _events.tsv file.
     Parameters:
@@ -23,18 +23,22 @@ def get_events_info(tsv_file:str):
         dict: dataset_name (str), subject (str), session (str), run (str)
     """
     tsv_file = tsv_file.replace("\\", "/")
-    pattern = r'^(?P<dataset_name>.+?/BIDS_[^/]+)/sub-(?P<subject>\d{2,3})/ses-(?P<session>\d{2})/eeg/sub-(?P=subject)_ses-(?P=session)_task-[^_]+_run-(?P<run>\d{2})_events\.tsv'
+    pattern = r"^(?P<dataset_name>.+?/BIDS_[^/]+)/sub-(?P<subject>\d{2,3})/ses-(?P<session>\d{2})/eeg/sub-(?P=subject)_ses-(?P=session)_task-[^_]+_run-(?P<run>\d{2})_events\.tsv"
     match = re.search(pattern, tsv_file)
 
     if match:
         # Extract values
-        dataset_name = match.group('dataset_name')
-        subject = match.group('subject')
-        session = match.group('session')
-        run = match.group('run')
-        
-        return {'dataset_name': dataset_name,
-                'subject': subject, 'session': session,'run': run}
+        dataset_name = match.group("dataset_name")
+        subject = match.group("subject")
+        session = match.group("session")
+        run = match.group("run")
+
+        return {
+            "dataset_name": dataset_name,
+            "subject": subject,
+            "session": session,
+            "run": run,
+        }
     else:
         print("No match found.")
         return None
@@ -48,7 +52,7 @@ def extract_sz_events(tsv_file):
     Returns:
         pl.DataFrame: A Polars DataFrame containing only seizure events, where eventType contains 'sz'.
     """
-    events_df = pl.read_csv(tsv_file, separator='\t')
+    events_df = pl.read_csv(tsv_file, separator="\t")
     sz_events_df = events_df.filter(events_df["eventType"].str.contains("sz"))
     return sz_events_df
 
@@ -67,14 +71,15 @@ def update_seizure_labels(df, sz_events_df):
         onset = int(event[sz_events_df.columns.index("onset")])
         duration = int(event[sz_events_df.columns.index("duration")])
         offset = onset + duration
-    
+
         df = df.with_columns(
-            pl.when((df['second'] >= onset) & (df['second'] <= offset))
+            pl.when((df["second"] >= onset) & (df["second"] <= offset))
             .then(True)
-            .otherwise(df['label'])
-            .alias('label')
+            .otherwise(df["label"])
+            .alias("label")
         )
     return df
+
 
 def select_train_patients(patients: list, ratio=0.75):
     """
@@ -86,7 +91,7 @@ def select_train_patients(patients: list, ratio=0.75):
     """
     if not patients:
         return []
-    
+
     # TODO make sure testing set has 'enough' seizures
     train_patients = random.sample(patients, int(len(patients) * ratio))
     return train_patients
@@ -127,60 +132,59 @@ def generate_labeled_df(tsv_file, dataset):
     assert Path(tsv_path).resolve() == Path(config_path).resolve(), 'Paths do not match'
     """
 
-    subject = info['subject']
-    session = info['session']
-    run = info['run']
-    unique_id = f'{dataset}_{subject}_{session}_{run}'
+    subject = info["subject"]
+    session = info["session"]
+    run = info["run"]
+    unique_id = f"{dataset}_{subject}_{session}_{run}"
     seizure_label = False
-    
+
     sz_events = extract_sz_events(tsv_file)
     if sz_events.shape[0]:
-        recording_duration = int(sz_events[0, 'recordingDuration'])
-        recording_dt = datetime.strptime(sz_events[0, 'dateTime'], "%Y-%m-%d %H:%M:%S")
+        recording_duration = int(sz_events[0, "recordingDuration"])
+        recording_dt = datetime.strptime(sz_events[0, "dateTime"], "%Y-%m-%d %H:%M:%S")
     else:
-        events = pl.read_csv(tsv_file, separator='\t')
-        recording_duration = int(events[0, 'recordingDuration'])
-        recording_dt = datetime.strptime(events[0, 'dateTime'], "%Y-%m-%d %H:%M:%S")
+        events = pl.read_csv(tsv_file, separator="\t")
+        recording_duration = int(events[0, "recordingDuration"])
+        recording_dt = datetime.strptime(events[0, "dateTime"], "%Y-%m-%d %H:%M:%S")
 
     seconds = list(range(1, recording_duration + 1))
-    timestamp = [recording_dt + timedelta(seconds=i) for i in range(1, recording_duration + 1)]
-    assert len(timestamp) == recording_duration, 'Timestamp array length does not match recording duration.'
-    
-    df = pl.DataFrame({
-    "unique_id": [unique_id] * recording_duration,
-    "subject": [subject] * recording_duration,
-    "session": [session] * recording_duration,
-    "run": [run] * recording_duration,
-    "dataset_name": [dataset_name] * recording_duration,
-    "second": seconds,
-    "timestamp": timestamp,
-    "label": [seizure_label] * recording_duration,
-    "training": [False] * recording_duration
-    })
+    timestamp = [
+        recording_dt + timedelta(seconds=i) for i in range(1, recording_duration + 1)
+    ]
+    assert (
+        len(timestamp) == recording_duration
+    ), "Timestamp array length does not match recording duration."
+
+    df = pl.DataFrame(
+        {
+            "unique_id": [unique_id] * recording_duration,
+            "subject": [subject] * recording_duration,
+            "session": [session] * recording_duration,
+            "run": [run] * recording_duration,
+            "dataset_name": [dataset_name] * recording_duration,
+            "second": seconds,
+            "timestamp": timestamp,
+            "label": [seizure_label] * recording_duration,
+            "training": [False] * recording_duration,
+        }
+    )
 
     if sz_events.shape[0]:
         lb_df = update_seizure_labels(df, sz_events)
         return lb_df
-    else: 
+    else:
         return df
 
 
 def main():
-    # Load BIDS dataset
-    with Path("config.toml").open("rb") as f:
-        config = tomllib.load(f)
-
-    bids_datasets = s.BIDS_DATASETS.keys()
-    
     final_df = None
 
-    for db in bids_datasets:
+    for db, bids_directory in s.BIDS_DATASETS.items():
         df = None
-        bids_directory = config['datasets'][db] 
 
-        db_tsv_files = pull_data.get_bids_file_paths(bids_dir=bids_directory,
-                                                     extension='tsv',
-                                                     data_type='events')
+        db_tsv_files = pull_data.get_bids_file_paths(
+            bids_dir=bids_directory, extension="tsv", data_type="events"
+        )
 
         for tsv_file in db_tsv_files:
             labeled_df = generate_labeled_df(tsv_file, db)
@@ -189,8 +193,8 @@ def main():
                 df = labeled_df
             else:
                 df = pl.concat([df, labeled_df], how="vertical")
-            
-        patients = df.select(pl.col('subject').unique()).to_series().to_list()
+
+        patients = df.select(pl.col("subject").unique()).to_series().to_list()
         train_patients = select_train_patients(patients)
         df = update_train_label(df, train_patients)
 
@@ -200,11 +204,9 @@ def main():
             final_df = pl.concat([final_df, df], how="vertical")
 
     # Save to a Parquet file
-    output_dir = Path(config['output']['out_dir'])
-    output_file = output_dir / 'output_dataset_pathlib.parquet'
-    final_df.write_parquet(output_file)
-    
+    final_df.write_parquet(s.LABELS_FILE)
     print("Parquet file has been generated successfully.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
