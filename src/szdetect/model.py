7dab639e306f8fe42e8a3c498e2f0ddacd81cb2f
oumgharbi <oumayma.gharbi@umontreal.ca>
2025-01-16 17:54:49 -0500

    Add calculate_metrics function to modules

--- src/szdetect/model.py
+++ src/szdetect/model.py
@@ -18,6 +18,8 @@ import yaml
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.metrics import precision_recall_fscore_support, precision_recall_curve
 #from sklearn import metrics
 #from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler#, OneHotEncoder
@@ -103,8 +105,112 @@ def init_models(in_dict, instance):
             ]
     return in_dict
 
+def calculate_metrics(model, test_set, scaled_X_test,
+                      tau=6, threshold=.85, step=1,
+                      show=True, home_path=home):
+    """
+    Estimate class of the input test set samples given a trained model,
+    implement regularization using firing power method and evaluate performance.
+    Args:
+        model        : trained model for predictions
+        test_set     : test or validation set to be used
+        scaled_X_test: scaled testing features
+        tau          : (regularization hyperparameter) window size for averaging firing power
+        threshold    : (regularization hyperparameter) minimum fraction of "full" firing power to raise an alarm
+        step         : step that was used for feature extraction
+        show         : bool - if True, plots features and prediction per seizure in the given set
+        home_path    : str, home directory for the EDF and features
+    Returns:
+        dict of metrics to evaluate the model
+    """
 
-def cross_validate(model, hyperparams, data, k, inner_k,
+    y_test = test_set.iloc[:, -1]
+    latencies = []
+    TP_SZ_overlap = []
+    y_hat = []
+    ovlp_precision, ovlp_recall, ovlp_f1 = [], [], []
+    ovlp_FA, ovlp_MA = 0, 0
+    FP_h, TP_h, FN_h = [], [], []
+    TP_duration, FN_duration = [], []
+    tiw, percentage_tiw = [], []
+    FAR = []
+
+    for i in test_set.Record_index.unique():
+        # Predictions per record _______________________________________________________________________________________
+        pred = predictions_per_record(test_set, record_row=i, model=model,
+                                      scaled_X_test=scaled_X_test,
+                                      tau=tau, threshold=threshold, step=step,
+                                      plot_alarms=show, show_plots=show,
+                                      home_path=home_path)  # ;
+
+        # Event-based metrics
+        OVLP = pred['OVLP']
+        ovlp_precision.append(OVLP['precision'])
+        ovlp_recall.append(OVLP['recall'])
+        ovlp_f1.append(OVLP['f1'])
+        ovlp_FA += OVLP['FP']
+        ovlp_MA += OVLP['FN']
+
+        latencies.extend(pred['Detection_latency'])
+        TP_SZ_overlap.extend(pred['TP_SZ_overlap'])
+        FAR.append(pred['FAR'])
+        #FP_h.extend(pred['FP_hours'])
+        #TP_h.extend(pred['TP_hours'])
+        #FN_h.extend(pred['FN_hours'])
+        tiw.extend(pred['Time_in_warning'])
+        #TP_duration.extend(pred['TP_duration'])
+        #FN_duration.extend(pred['FN_duration'])
+        percentage_tiw.append(pred['percentage_tiw'])
+        y_hat.extend(pred['regularized_predictions'])
+
+    # L = [1 if x >= threshold else 0 for x in y_hat]
+    # sample-based, on all test dataset
+    try:
+        assert len(y_test) == len(y_hat)
+        pres_rg, rec_rg, f1_rg, support_rg = precision_recall_fscore_support(y_test, y_hat,
+                                                                             zero_division=0)
+        s = len(support_rg)
+        f1_rg = 100 * float("{:.4f}".format(f1_rg[s - 1]))
+        pres_rg = 100 * float("{:.4f}".format(pres_rg[s-1]))
+        rec_rg = 100 * float("{:.4f}".format(rec_rg[s-1]))
+
+        if model.__class__.__name__ in ['LogisticRegression', 'SVC', 'XGBClassifier']:
+            # y_pred_score = model.decision_function(scaled_X_test)
+            # roc = roc_auc_score(y_test, y_pred_score)  # sample-based
+            y_pred_score = model.predict_proba(scaled_X_test)[:, 1]
+            roc = roc_auc_score(y_test, y_pred_score)  # sample-based
+            fpr, tpr, thresholds = roc_curve(y_test, y_pred_score)  # sample-based
+        else:
+            roc = np.nan
+            fpr, tpr, thresholds = np.array([]), np.array([]), np.array([])
+
+    except (AssertionError, ValueError) as error:
+        logger.error(f'ERROR {error} len(true_ann)={len(y_test)}, len(pred_ann)={len(y_hat)}',
+                     exc_info=True)
+        f1_rg = np.nan
+        pres_rg = np.nan
+        rec_rg = np.nan
+        roc = np.nan
+        fpr, tpr, thresholds = np.array([]), np.array([]), np.array([])
+
+    return {'f1_score_regularized': f1_rg,  # sample-based
+            'precision_regularized': pres_rg,  # sample-based
+            'recall_regularized': rec_rg,  # sample-based
+            'roc_auc_score': float("{:.4f}".format(roc)),  # sample-based
+            'roc_fpr': fpr, 'roc_tpr': tpr, 'roc_thresholds': thresholds,  # sample-based
+            'ovlp_precision': 100 * float("{:.4f}".format(np.nanmean(ovlp_precision))),  # event-based
+            'ovlp_recall': 100 * float("{:.4f}".format(np.nanmean(ovlp_recall))),  # event-based
+            'ovlp_f1': 100 * float("{:.4f}".format(np.nanmean(ovlp_f1))),  # event-based
+            'ovlp_FA': ovlp_FA, 'ovlp_MA': ovlp_MA,  # event-based
+            'latencies': latencies,  # event-based
+            #'TP_SZ_overlap': TP_SZ_overlap,  # event-based
+            'FAR': FAR,  # event-based
+            'Time_in_warning': tiw, 'percentage_tiw': percentage_tiw,  # event-based
+            #'TP_duration': TP_duration, 'FN_duration': FN_duration,  # event-based
+            #'FP_hours': FP_h, 'TP_hours': TP_h, 'FN_hours': FN_h  # event-based
+            }
+
+def cross_validate(model, hyperparams:list, data:pl.DataFrame, k:int, inner_k:int,
                    home_path=home):
     """Get CV score with an inner CV loop to select hyperparameters.
 
@@ -131,20 +237,24 @@ def cross_validate(model, hyperparams, data, k, inner_k,
     df_roc_curves = pl.DataFrame([])
 
     clf = model.__class__.__name__
+    feature_columns = []  # TODO get feature column names
+    # nonfeature_columns = ['unique_id', 'subject', 'session', 'run', 'dataset_name', 'second', 'timestamp' 'label', 'training']
+        
 
-    win_size = hyperparams[0][2]
-    step = hyperparams[0][3]
+    #win_size = hyperparams[0][2]
+    #step = hyperparams[0][3]
     all_scores = []
 
     for i, (train_idx, test_idx) in enumerate(get_train_test_splits(data[['Patient_ID']], k)):
         
         logger.info(f"\nOuter CV loop: fold {i+1}")
 
-        train_val_set = data.iloc[train_idx]
-        test_set = data.iloc[test_idx]
+        train_val_set = data[train_idx]
+        test_set = data[test_idx]
 
-        X_test = test_set.iloc[:, 4:-1]
-        # y_test = test_set.iloc[:, -1]
+        X_test = test_set.select([pl.col(col) for col in feature_columns])
+        # or exclude non-features columns
+        #X_test = test_set.select([col for col in test_set.columns if col not in nonfeature_columns])
 
         gridsearch_per_fold = grid_search(model, hyperparams, train_val_set, inner_k,
                                           outer_fold_idx=i,
@@ -160,19 +270,19 @@ def cross_validate(model, hyperparams, data, k, inner_k,
         
         # Make predictions and calculate performance metrics
         metrics = calculate_metrics(best_model, test_set, scaled_X_test=X_test,
-                                    tau=best_hp[4], threshold=best_hp[5], step=best_hp[3],
+                                    tau=best_hp[4], threshold=best_hp[5],
                                     show=False)
         
         results_rows.append(
             {
             'fold': i+1,
-            'train_Patient_ID': list(train_val_set.Patient_ID.unique()),
-            'test_Patient_ID': list(test_set.Patient_ID.unique()),
+            'train_Patient_ID': train_val_set.select(pl.col("subject").unique()).to_series().to_list(),
+            'test_Patient_ID': test_set.select(pl.col("subject").unique()).to_series().to_list(),
             'model': model_name,
             'max_depth': best_hp[0],
             'min_child_weight': best_hp[1],
-            'win_size': best_hp[2],
-            'step': best_hp[3],
+            #'win_size': best_hp[2],
+            #'step': best_hp[3],
             'tau': best_hp[4],
             'threshold': best_hp[5],
             'avg_latency': np.nanmean(np.array(metrics['latencies'])),
