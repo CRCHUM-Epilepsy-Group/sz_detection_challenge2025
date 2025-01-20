6769c964ae008c07db92091a0d3a0e17ebd1e0d9
oumgharbi <oumayma.gharbi@umontreal.ca>
2025-01-20 16:01:51 -0500

    Load new test feature file

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -1,10 +1,9 @@
 import polars as pl
-import duckdb
+#import duckdb
 from pathlib import Path
 
 #from szdetect import model as mod
 
+features_file = Path('./test_data/features/tuh_sz_bids_007_00_00.parquet')
+features = pl.read_parquet(features_file)
 
-features = pl.read_parquet(Path('./test_data/features.parquet'))
-
-features_db = duckdb.sql('')
