d73c13652c9c9a2f7a54cbaa1771c1a0e194ce2e
oumgharbi <oumayma.gharbi@umontreal.ca>
2025-01-23 15:00:20 -0500

    Use pull_features to load features parquet files

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -1,9 +1,17 @@
 import polars as pl
-#import duckdb
 from pathlib import Path
+from szdetect import pull_features as pf
 
-#from szdetect import model as mod
 
-features_file = Path('./test_data/features/tuh_sz_bids_007_00_00.parquet')
-features = pl.read_parquet(features_file)
+# features_file = Path('./test_data/features/tuh_sz_bids_007_00_00.parquet')
+# labels_file = Path('./test_data/labels.parquet')
+# features = pl.read_parquet(features_file)
+# labels = pl.read_parquet(labels_file)
+
+df = pf.pull_features(
+    feature_dir=Path('./test_data/features'),
+    label_file='./test_data/labels.parquet',
+    feature_group="efficiency",
+    train_only=True)
+
 
