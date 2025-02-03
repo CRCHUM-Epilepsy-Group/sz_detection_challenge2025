1b3a7532afaf90af5f654282e7ffb64262155963
Oumayma Gharbi <oumaymagharbi.og@gmail.com>
2025-02-03 13:13:14 -0500

    Change LABELS_FILE to str to avoid Path-related error in pull_features

--- src/szdetect/project_settings.py
+++ src/szdetect/project_settings.py
@@ -21,8 +21,8 @@ BIDS_DB_FILES_DIR = Path(config["utilities"]["bids_db_files_dir"])
 BIDS_DB_FILES_DIR.mkdir(parents=True, exist_ok=True)
 
 # Labels
-LABELS_FILE = Path(config["labels"]["labels_file"])
-LABELS_FILE.parent.mkdir(parents=True, exist_ok=True)
+LABELS_FILE = config["labels"]["labels_file"]
+Path(LABELS_FILE).parent.mkdir(parents=True, exist_ok=True)
 
 # Feature extraction
 FEATURES_DB = config["features"]["features_db_file"]
