9115a6b9727f282da912a80609f2d1f86d6fbabf
Oumayma Gharbi <oumaymagharbi.og@gmail.com>
2025-02-03 13:11:06 -0500

    Load features and labels according to config variables

--- tests/test_pivot.py
+++ tests/test_pivot.py
@@ -1,13 +1,16 @@
 import polars as pl
 from pathlib import Path
 from szdetect import pull_features as pf
+from szdetect import project_settings as s
 
 
 df = pf.pull_features(
-    feature_dir=Path('./test_data/features'),
-    label_file='./test_data/labels.parquet',
-    feature_group="all",
+    feature_dir=s.FEATURES_DIR,
+    label_file=s.LABELS_FILE,
+    feature_group="efficiency",
     train_only=True)
+
+print('Features pulled')
 
 index_col = ['epoch', 'timestamp', 'dataset_name', 'subject',
              'session', 'run', 'unique_id', 'second', 'label']
@@ -23,8 +26,14 @@ long_df = long_df.with_columns([
 
 long_df = long_df.filter(pl.col('feature') !='band_power')
 
+print('band_power features filtered out')
+
 wide_df = long_df.pivot(
     values='value', 
     index=index_col, 
     on=feature_col,
 )
+print('Long to wide pivot succeeded.')
+
+print('Nb of FALSE labels', len(wide_df.filter(pl.col('label')==False)))
+print('Nb of TRUE labels', len(wide_df.filter(pl.col('label')==True)))
